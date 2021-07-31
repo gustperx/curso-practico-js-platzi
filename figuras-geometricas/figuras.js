37e55fe91c9ea865fcd6001ef59e54e9afe3d74c
gustperx <gtgustavo20@gmail.com>
2021-07-31 16:23:25 -0400

    first commit

---
+++ figuras-geometricas/figuras.js
@@ -0,0 +1,53 @@
+// Código del cuadrado
+console.group("Cuadrados");
+const ladoCuadrado = 5;
+console.log(`Los lados del cuadrado miden: ${ladoCuadrado}cm`);
+
+const perimetroCuadrado = ladoCuadrado * 4;
+console.log(`El perímetro del cuadrado: ${perimetroCuadrado}cm`);
+
+const areaCuadrado = ladoCuadrado * ladoCuadrado;
+console.log(`El área del cuadrado es: ${areaCuadrado}cm*2`);
+console.groupEnd();
+
+// Código del triángulo
+console.group("Triángulos");
+const ladoTriangulo1 = 6;
+const ladoTriangulo2 = 6;
+const baseTriangulo = 4;
+const alturaTriangulo = 5.5;
+console.log(
+  `Los lados del triángulo miden: ${ladoTriangulo1}cm, ${ladoTriangulo2}cm ${baseTriangulo}cm`
+);
+console.log(`La altura del triángulo es de: ${alturaTriangulo}cm`);
+
+const perimetroTriangulo = ladoTriangulo1 + ladoTriangulo2 + baseTriangulo;
+console.log(`El perímetro del tríangulo es: ${perimetroTriangulo}cm`);
+
+const areaTriangulo = (baseTriangulo * alturaTriangulo) / 2;
+console.log(`El área del triángulo es: ${areaTriangulo}cm*2`);
+console.groupEnd();
+
+// Código del circulo
+console.group("Circulo");
+
+// Radio
+const radioCirculo = 4;
+console.log(`El radio del círculo es: ${radioCirculo}cm`);
+
+// Diámetro
+const diametroCirculo = radioCirculo * 2;
+console.log(`El diámetro del círculo es: ${diametroCirculo}cm`);
+
+// PI
+const PI = Math.PI;
+console.log(`PI es: ${PI}`);
+
+// Circunferencia
+const perimetroCirculo = diametroCirculo * PI;
+console.log(`El perímetro del círculo es: ${perimetroCirculo}cm`);
+
+// Area
+const areaCirculo = radioCirculo * radioCirculo * PI;
+console.log(`El área del círculo es: ${areaCirculo}cm`);
+console.groupEnd();
---
+++ readme.md
@@ -0,0 +1,10 @@
+# Curso Practico de JavaScrip
+
+...
+
+## Taller #1: Fuguras geométricas
+
+- Primer paso: Definir las fórmulas
+- Segundo paso: Implementar formulas en JavaScript
+- Tercer paso: Crear funciones
+- Cuarto paso: Integrar JS con HTML
